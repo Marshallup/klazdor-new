407b2d5483f81a05fb51198f5c7c72a016bd0455
Mikhail Teslya <televonvea@gmail.com>
2021-05-30 16:01:31 +0300

    sdsd

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -121,13 +121,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
         const $this = $('.header-catalog-btn');
         const className = 'header-catalog-btn--active';
         const bigMenuInner = $('.header-big-menu-inner');
-        const bigMenu = $('.header-big-menu');
+        const bigMenu = $('.header-big-menu-item-wrap');
 
         $this.toggleClass(className);
 
         if (!$this.hasClass(className)) {
             bigMenu.fadeOut(300, function() {
               bigMenuInner.hide();
+              hideAllListMenu();
               animPending = false;
             });
         } else {
@@ -139,6 +140,44 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
       }
     });
+
+
+    // МОБИЛЬНОЕ МЕНЮ
+
+
+    const mobileListParent = $('.header-big-menu-list__item--parent');
+
+    const headerListActiveClass = 'header-big-menu-list__item--active';
+
+    mobileListParent.click(function(event) {
+
+      const targetElem = $(event.target);
+
+      if (targetElem.hasClass('header-big-menu-list__link')) return;
+      event.stopPropagation();
+
+
+      
+      const $this = $(this);
+
+      const childList = $this.children('.header-big-menu-list');
+
+      if (!$this.closest('.' + headerListActiveClass).length) {
+        hideAllListMenu(childList);
+      }
+
+      $this.toggleClass(headerListActiveClass);
+
+
+      if ($this.hasClass(headerListActiveClass)) {
+        childList.slideDown(300);
+      } else {
+        hideAllListMenu(childList);
+      }
+
+    });
+
+
     let footerList = $('.footer-cell').children('.footer-list');
 
     footerList.on('click', function(e) {
@@ -247,5 +286,113 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 
     };
+    
+
+    function hideAllListMenu(childList) {
+      const parentList = $('.header-big-menu-list')
+      const allItemChild =  parentList.find('.header-big-menu-list__item--parent');
+        const allListChild = parentList.find('.header-big-menu-list');
+        if (allItemChild.length > 0) {
+          allItemChild.removeClass(headerListActiveClass);
+          allListChild.slideUp(300);
+        }
+        if (childList)  {childList.slideUp(300);}
+    };
+
+
+
+
+
+
+    //Обработка клика на стрелку вправо
+    $(document).on('click', ".carousel-footer-button-right",function(){
+      var carusel = $(this).parents('.carousel-footer');
+      updtae_reviews();
+      right_carusel(carusel);
+
+      return false;
+  });
+  //Обработка клика на стрелку влево
+  $(document).on('click',".carousel-footer-button-left",function(){
+      var carusel = $(this).parents('.carousel-footer');
+      updtae_reviews();
+      left_carusel(carusel);
+
+      return false;
+  });
+  function left_carusel(carusel){
+      var block_width = $(carusel).find('.carousel-footer-block').outerWidth();
+      $(carusel).find(".carousel-footer-items .carousel-footer-block").eq(-1).clone().prependTo($(carusel).find(".carousel-footer-items"));
+      $(carusel).find(".carousel-footer-items").css({"left":"-"+block_width+"px"});
+      $(carusel).find(".carousel-footer-items .carousel-footer-block").eq(-1).remove();
+      $(carusel).find(".carousel-footer-items").animate({left: "0px"}, 200);
+  }
+  function right_carusel(carusel){
+      var block_width = $(carusel).find('.carousel-footer-block').outerWidth();
+      $(carusel).find(".carousel-footer-items").animate({left: "-"+ block_width +"px"}, 200, function(){
+          $(carusel).find(".carousel-footer-items .carousel-footer-block").eq(0).clone().appendTo($(carusel).find(".carousel-footer-items"));
+          $(carusel).find(".carousel-footer-items .carousel-footer-block").eq(0).remove();
+          $(carusel).find(".carousel-footer-items").css({"left":"0px"});
+      });
+  }
+  function updtae_reviews() {
+
+      var ids = [];
+      var last_one;
+      $('[data-id]').each( function(e){
+          var $el = $(this);
+          if($el.hasClass('carousel-footer-block')) {
+              ids.push($el.attr('data-id'));
+          }
+
+      });
+
+      last_one = Math.min.apply(null, ids);
+
+      // jQuery.ajax({
+      //     url: BX.message('TEMPLATE_PATH') + '/new_review.php',
+      //     type: 'post',
+      //     data: "id=" + ids,
+      //     success: function(res) {
+
+      //         var response = JSON.parse(res);
+
+      //         $('.carousel-footer-items').append(response);
+
+      //     },
+      //     error: function(e) {
+
+      //         console.log(e);
+      //     }
+      // });
+  }
+
+  /* сокращаем Достоинства и Недостатки до 35 символов + троеточие */
+  jQuery(".short .n-product-review-item__text").each(function() {
+      var review = jQuery(this).html();
+      if(review.length > 35)
+      {
+          review = review.substring(0, 35) + "...";
+          jQuery(this).html(review);
+      }
+  });
+
+  /* чтобы отзывы были примерно одной высоты (около 150 символов):
+  *  смотрим общее число знаков отзыва
+  *  вычисляем какое кол-во от этого занимает комментарий
+  *  от 150 отнимаем кол-во знаков отзыва без учета комментария
+  *  уменьшаем длину комментария до получившегося в предыдущем действии значения
+  */
+  jQuery(".short .n-product-review-item__text").each(function() {
+    var review = jQuery(this).html();
+    if(review.length > 35)
+    {
+        review = review.substring(0, 35) + "...";
+        jQuery(this).html(review);
+    }
+});
+
+
+
 
 });
